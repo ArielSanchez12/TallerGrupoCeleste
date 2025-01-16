5c65c07518b6fbe47e685eb99e14a4f72107b955
Diego Mullo <diego.mullo@epn.edu.ec>
2025-01-16 09:09:38 -0500

    login conectado a la bdd

--- src/conexion.java
+++ src/conexion.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 public class conexion {
 
-    private static final String URL = "mysql://uwfx3wgrfnj9px2s:x6xJuDOr496wThYiOPUr@bsmvfr83jdr81zezcwxq-mysql.services.clever-cloud.com:3306/bsmvfr83jdr81zezcwxq";
+    private static final String URL = "jdbc:mysql://uwfx3wgrfnj9px2s:x6xJuDOr496wThYiOPUr@bsmvfr83jdr81zezcwxq-mysql.services.clever-cloud.com:3306/bsmvfr83jdr81zezcwxq";
     private static final String USER = "uwfx3wgrfnj9px2s";
     private static final String PASSWORD = "x6xJuDOr496wThYiOPUr";
 
--- src/login.java
+++ src/login.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class login extends JFrame{
     private JButton ingresarButton;
@@ -8,8 +11,40 @@ public class login extends JFrame{
 
     public login(){
         setTitle("Login");
-        setSize(500,500);
+        setSize(500, 500);
         setContentPane(panelLogin);
         setVisible(true);
+
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nombre_usuario = textField1.getText();
+                String password = new String(passwordField1.getPassword());
+
+
+
+                if (validarUsuario(nombre_usuario, password)) {
+                    JOptionPane.showMessageDialog(null, "Ingresaste con exito");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Credenciales incorrectas");
+                }
+            }
+        });
     }
+    private boolean validarUsuario(String nombre_usuario, String password){
+        try(Connection con = conexion.connect()){
+            String query ="SELECT * FROM usuarios WHERE nombre_usuario=? AND password=?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1,nombre_usuario);
+            ps.setString(2,password);
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,"No se pudo conectar a la base de datos");
+            return false;
+        }
+    }
+
 }
