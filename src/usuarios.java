34f2d88b1e2b0c1f5161c71389b46eda7c3e3729
Diego Mullo <diego.mullo@epn.edu.ec>
2025-01-16 08:09:00 -0500

    Pestaña usuario nueva

--- src/usuarios.java
+++ src/usuarios.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 
-public class usuarios {
+public class usuarios extends JFrame {
     private JTabbedPane tabMenor;
     private JPanel panel1;
     private JPanel tabTodo;
     private JList list1;
+    private JList list3;
     private JList list2;
+
+
 }
