706af2bd01330f19e0ed266d86498b6e3ac0c025
dannamishelle23 <dannamishelle.23@gmail.com>
2025-01-16 21:16:35 -0500

    LOGIN CORREGIDO

--- src/login.java
+++ src/login.java
@@ -1,47 +1,95 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class login extends JFrame{
-    private JButton ingresarButton;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JPanel panelLogin;
+public class login extends conexion {
+    public JTextField textField1;
+    public JPasswordField passwordField1;
+    public JComboBox comboBox1;
+    public JButton ingresarButton;
+    public JPanel Plogin;
 
-    public login(){
-        setTitle("Login");
-        setSize(500, 500);
-        setContentPane(panelLogin);
-        setVisible(true);
+    public login() {
+        JFrame menuFrame = new JFrame("Login");
+        menuFrame.setContentPane(Plogin);
+        menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuFrame.setSize(500, 300);
+        menuFrame.setPreferredSize(new Dimension(500, 300));
+        menuFrame.setLocationRelativeTo(null);
+        menuFrame.pack();
+        menuFrame.setVisible(true);
+        comboBox1.addItem("administrador");
+        comboBox1.addItem("usuario");
+
 
         ingresarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String nombre_usuario = textField1.getText();
-                String password = new String(passwordField1.getPassword());
+                String usuario = textField1.getText();
+                String contrasenia = new String(passwordField1.getPassword());
+                String rolSeleccionado = (String) comboBox1.getSelectedItem();
 
-                if (validarUsuario(nombre_usuario, password)) {
-                    JOptionPane.showMessageDialog(null, "Ingresaste con exito");
-                } else {
-                    JOptionPane.showMessageDialog(null, "Credenciales incorrectas");
+                if (usuario.isEmpty() || contrasenia.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Por favor, complete todos los campos");
+                    return;
+                }
+
+                try (Connection conn = connect()) {
+                    String sql = "SELECT rol FROM usuarios WHERE nombre_usuario = ? AND password = ?";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, usuario);
+                    pstmt.setString(2, contrasenia);
+
+                    ResultSet resultSet = pstmt.executeQuery();
+
+                    if (resultSet.next()) {
+                        String rol = resultSet.getString("rol");
+
+                        if (rol.equals(rolSeleccionado)) {
+                            JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso! Bienvenido " + usuario);
+
+                            if (rol.equals("administrador")) {
+
+                                JFrame menuFrame = new JFrame("Pantalla de Administrador");
+                                menuFrame.setContentPane(new administrador().ventana_administrador);
+                                menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                menuFrame.setSize(500, 300);
+                                menuFrame.setPreferredSize(new Dimension(500, 300));
+                                menuFrame.setLocationRelativeTo(null);
+                                menuFrame.pack();
+                                menuFrame.setVisible(true);
+
+                            }else if (rol.equals("usuario")) {
+
+                                JFrame menuFrame = new JFrame("Pantalla de Usuario");
+                                menuFrame.setContentPane(new usuarios().ventana_usuario);
+                                menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                menuFrame.setSize(500, 300);
+                                menuFrame.setPreferredSize(new Dimension(500, 300));
+                                menuFrame.setLocationRelativeTo(null);
+                                menuFrame.pack();
+                                menuFrame.setVisible(true);
+
+                            }else {
+                                JOptionPane.showMessageDialog(null, "No existe ese usuario en el rol actual, prueba con otro rol");
+                            }
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "El usuario: " + usuario + " no existe en el rol seleccionado");
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Error en clever cloud");
                 }
             }
         });
     }
-    private boolean validarUsuario(String nombre_usuario, String password){
-        try(Connection con = conexion.connect()){
-            String query ="SELECT * FROM usuarios WHERE nombre_usuario=? AND password=?";
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1,nombre_usuario);
-            ps.setString(2,password);
-            ResultSet rs = ps.executeQuery();
-            return rs.next();
-        }
-        catch(Exception e){
-            JOptionPane.showMessageDialog(null,"No se pudo conectar a la base de datos");
-            return false;
-        }
-    }
-
 }
