9d64892016f2643c3c204f10cf83e062d8a94f36
2Goddie7 <diego.mullo@epn.edu.ec>
2025-01-16 21:21:02 -0500

    clase administrador completa

--- src/administrador.java
+++ src/administrador.java
@@ -1,146 +1,188 @@
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.function.IntToDoubleFunction;
 
-public class administrador extends Component {
-    private JPanel ventana_administrador;
-    private JTabbedPane pestañas;
-    private JTextField producto1;
-    private JTextField cantidad1;
-    private JTextField nuevo_usuario;
-    private JTextField pass_new_user;
-    private JTextArea textArea1;
-    private JButton view_productos;
-    private JButton view_users;
-    private JTextArea textArea2;
-    private JButton add_productos;
-    private JButton add_user;
-    private JPanel agregar_usuario1;
-    private JTextField descripcion;
-    private JTextField precio;
-    private JTextField new_rol;
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("administrador");
-        frame.setContentPane(new administrador().ventana_administrador);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
+public class administrador extends conexion{
+    public JPanel ventana_administrador;
+    public JTabbedPane tabbedPane1;
+    public JTextField nombreProducto;
+    public JTextField textField2;
+    public JTextField textField3;
+    public JTextField textField4;
+    public JButton agregarProductosButton;
+    public JTextField textField5;
+    public JTextField textField7;
+    public JPasswordField passwordField1;
+    public JButton agregarUsuario;
+    public JTable table1;
+    public JTable table2;
+    public JButton mostrarListaDeProductosButton;
+    public JButton mostrarListaDeUsuariosButton;
+    public JPanel Pproductos;
 
     public administrador() {
-        add_productos.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                agregar_productos();
-            }
-        });
-
-        add_user.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                agregar_usuario();
-            }
-        });
-        view_productos.addActionListener(new ActionListener() {
+        agregarProductosButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String nombreProductoText = nombreProducto.getText();
+                String descripcionText = textField2.getText();
+                String precioText = textField3.getText();
+                String stockText = textField4.getText();
+
+                if (nombreProductoText.isEmpty() || descripcionText.isEmpty() || precioText.isEmpty() || stockText.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Por favor, complete todos los campos");
+                    return;
+                }
+
+                double precioDecimal = Double.parseDouble(textField3.getText());
+                int stockEntero = Integer.parseInt(textField4.getText());
+
+                if (stockEntero < 10 || stockEntero > 350) {
+                    JOptionPane.showMessageDialog(null, "El stock minimo es 10 y el maximo es 350");
+                    return;
+                }
+
+                try (Connection conn = connect()) {
+                    String sql = "INSERT INTO productos (nombre, descripcion, precio, stock) VALUES (?, ?, ?, ?)";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, nombreProductoText);
+                    pstmt.setString(2, descripcionText);
+                    pstmt.setDouble(3, precioDecimal);
+                    pstmt.setInt(4, stockEntero);
+                    pstmt.execute();
+                    JOptionPane.showMessageDialog(null, "Registro de productos exitoso");
+                    nombreProducto.setText("");
+                    textField2.setText("");
+                    textField3.setText("");
+                    textField4.setText("");
+
+                } catch (SQLException exception) {
+                    throw new RuntimeException(exception);
+                }
             }
         });
-        view_users.addActionListener(new ActionListener() {
+        agregarUsuario.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String nombreUser = textField5.getText();
+                String contra = passwordField1.getText();
+                String rolUser = textField7.getText();
 
+                if (nombreUser.isEmpty() || contra.isEmpty() || rolUser.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Por favor, complete todos los campos");
+                    return;
+                }
+
+                try (Connection conn = connect()) {
+                    String sql = "INSERT INTO usuarios (nombre_usuario, password, rol) VALUES (?, ?, ?)";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, nombreUser);
+                    pstmt.setString(2, contra);
+                    pstmt.setString(3, rolUser);
+                    pstmt.execute();
+                    JOptionPane.showMessageDialog(null, "Registro de usuario exitoso");
+                    textField5.setText("");
+                    passwordField1.setText("");
+                    textField7.setText("");
+
+                } catch (SQLException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+        });
+
+        mostrarListaDeProductosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try (Connection conn = connect()) {
+                    String sql = "SELECT id_producto, nombre, descripcion, precio, stock FROM productos";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    ResultSet resultSet = pstmt.executeQuery();
+
+                    // Configurar el modelo de la tabla
+                    String[] columnas = {"ID Producto", "Nombre", "Descripción", "Precio", "Stock"};
+                    DefaultTableModel modelo = new DefaultTableModel(columnas, 0) {
+                        @Override
+                        public boolean isCellEditable(int row, int column) {
+                            return false;
+                        }
+                    };
+
+                    // Procesar los resultados del ResultSet
+                    boolean hayResultados = false;
+                    while (resultSet.next()) {
+                        hayResultados = true;
+                        int idProducto = resultSet.getInt("id_producto");
+                        String nombre = resultSet.getString("nombre");
+                        String descripcion = resultSet.getString("descripcion");
+                        double precio = resultSet.getDouble("precio");
+                        int stock = resultSet.getInt("stock");
+
+                        // Agregar los datos al modelo
+                        Object[] fila = {idProducto, nombre, descripcion, precio, stock};
+                        modelo.addRow(fila);
+                    }
+
+                    if (!hayResultados) {
+                        JOptionPane.showMessageDialog(null, "No existen productos registrados");
+                    }
+
+                    // Asignar el modelo a la tabla
+                    table1.setModel(modelo);
+
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Error en clever cloud");
+                }
+            }
+        });
+
+        mostrarListaDeUsuariosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try (Connection conn = connect()) {
+                    String sql = "SELECT id_usuario, nombre_usuario, password, rol FROM usuarios";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    ResultSet resultSet = pstmt.executeQuery();
+
+                    String[] columnas = {"ID Usuario", "Nombre Usuario", "Contraseña", "Rol"};
+                    DefaultTableModel modelo2 = new DefaultTableModel(columnas, 0) {
+                        @Override
+                        public boolean isCellEditable(int row, int column) {
+                            return false; // Hacer las celdas no editables
+                        }
+                    };
+
+                    boolean hayResultados2 = false;
+                    while (resultSet.next()) {
+                        hayResultados2 = true;
+                        int idUser = resultSet.getInt("id_usuario");
+                        String nombreUser = resultSet.getString("nombre_usuario");
+                        String contraUser = resultSet.getString("password");
+                        String rol = resultSet.getString("rol");
+
+
+                        Object[] fila = {idUser, nombreUser, contraUser, rol};
+                        modelo2.addRow(fila);
+                    }
+
+                    if (!hayResultados2) {
+                        JOptionPane.showMessageDialog(null, "No existen usuarios registrados");
+                    }
+
+                    table2.setModel(modelo2);
+
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Error en clever cloud");
+                }
             }
         });
     }
-
-    public void agregar_productos(){
-        String nombreProducto = producto1.getText();  // Obtener nombre del producto
-        String descripcionProducto = descripcion.getText();  // Obtener descripción del producto
-        String precioProducto = precio.getText();
-        int cantidadProducto = Integer.parseInt(cantidad1.getText());  // Convertir cantidad a entero
-
-
-
-        String query = "INSERT INTO productos (nombre, descripcion, precio, stock) VALUES (?, ?, ?, ?)";
-
-        try (Connection connect = conexion.connect();  // Asegúrate de tener la clase Conexion correctamente configurada
-             PreparedStatement statement = connect.prepareStatement(query)) {
-
-            // Asignar los valores a los parámetros
-            statement.setString(1, nombreProducto);  // Nombre del producto
-            statement.setString(2, descripcionProducto);  // Descripción del producto
-            statement.setInt(3, cantidadProducto);
-            statement.setString(4, precioProducto);  // Precio del producto
-              // Stock (Cantidad)
-
-            // Ejecutar la consulta
-            statement.executeUpdate();
-
-            // Mostrar mensaje de éxito y recargar la lista de productos
-            JOptionPane.showMessageDialog(null, "Producto agregado exitosamente.");
-              // Recargar la lista de productos
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Error al agregar producto.");
-        }
-
-    }
-
-    public void agregar_usuario(){
-        String nombreUsuario = nuevo_usuario.getText();  // Obtener nombre de usuario
-        String passUsuario = pass_new_user.getText();  // Obtener contraseña de usuario
-        String nuevoRol = new_rol.getText(); // Obtener nuevo rol
-
-        // Verificar si los campos no están vacíos
-        if (nombreUsuario.isEmpty() || passUsuario.isEmpty() || nuevoRol.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Por favor complete todos los campos.");
-            return;  // Salir si algún campo está vacío
-        }
-
-        // Consulta SQL para insertar un usuario con rol
-        String query = "INSERT INTO usuarios (nombre, password, rol) VALUES (?, ?, ?)";
-
-        try (Connection connect = conexion.connect();  // Asegúrate de tener la clase Conexion correctamente configurada
-             PreparedStatement statement = connect.prepareStatement(query)) {
-
-            // Asignar los valores a los parámetros
-            statement.setString(1, nombreUsuario);  // Nombre del usuario
-            statement.setString(2, passUsuario);  // Contraseña del usuario
-            statement.setString(3, nuevoRol);  // Rol del usuario
-
-            // Ejecutar la consulta
-            statement.executeUpdate();
-
-            // Mostrar mensaje de éxito
-            JOptionPane.showMessageDialog(this, "Usuario agregado exitosamente.");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Error al agregar usuario.");
-        }
-
-    }
-
-    public void lista_productos(){
-
-
-
-    }
-
-    public void lista_usuarios(){
-
-    }
-
-
-
-
 }
